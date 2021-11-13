c5e1e0d320f6d42bbc22805143731cf2b919fd35
Jesse Feng <fxm0016@126.com>
2021-11-13 15:18:19 +0800

    Create ResizeObserver.ts

---
+++ src/ResizeObserver.ts
@@ -0,0 +1,31 @@
+import IResizeObserver, { IResizeObserverCallback, IResizeObserverEntry } from '~/types';
+import ResizeObserverSPI from './ResizeObserverSPI';
+
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+class ResizeObserverImpl implements IResizeObserver {
+  private readonly observer_: ResizeObserverSPI;
+
+  constructor(callback: IResizeObserverCallback) {
+    if (!(this instanceof ResizeObserverImpl)) {
+      throw new TypeError('Cannot call a class as a function.');
+    }
+    if (!arguments.length) {
+      throw new TypeError('1 argument required, but only 0 present.');
+    }
+
+    this.observer_ = new ResizeObserverSPI((entries: IResizeObserverEntry[]) => {
+      callback.call(this, entries, this);
+    });
+  }
+  observe(target: Element): void {
+    return this.observer_.observe(target);
+  }
+  unobserve(target: Element): void {
+    return this.observer_.unobserve(target);
+  }
+  disconnect(): void {
+    return this.observer_.disconnect();
+  }
+}
+
+export default ResizeObserverImpl;
