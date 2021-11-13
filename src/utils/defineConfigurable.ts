83fc9a60e5083f08470c5f5d6d8cb971b0558db3
Jesse Feng <fxm0016@126.com>
2021-11-13 15:20:27 +0800

    Create defineConfigurable.ts

---
+++ src/utils/defineConfigurable.ts
@@ -0,0 +1,13 @@
+import { forOwn } from 'celia';
+
+export default (target: any, props: any) => {
+  forOwn(props, (val, key) => {
+    Object.defineProperty(target, key, {
+      value: val,
+      enumerable: false,
+      writable: false,
+      configurable: true
+    });
+  });
+  return target;
+};
