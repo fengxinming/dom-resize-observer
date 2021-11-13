afe034aa2d01489ea0f3514b74af6812ffeaa44c
Jesse Feng <fxm0016@126.com>
2021-11-13 15:23:12 +0800

    Create Map.ts

---
+++ src/shims/Map.ts
@@ -0,0 +1,75 @@
+import { isUndefined, forEach } from 'celia';
+
+function findIndex(arr: any[], key: any) {
+  let result = -1;
+  forEach(arr, (entry, index) => {
+    if (entry[0] === key) {
+      result = index;
+      return false;
+    }
+  });
+  return result;
+}
+
+class MapShim {
+  private readonly entries_: any[] = [];
+
+  get size() {
+    return this.entries_.length;
+  }
+
+  clear(): void {
+    this.entries_.splice(0);
+  }
+
+  delete(key: any): boolean {
+    const { entries_ } = this;
+    const index = findIndex(entries_, key);
+
+    if (~index) {
+      return !!entries_.splice(index, 1)[0];
+    }
+    return false;
+  }
+
+  forEach(callbackfn: (value: any, key: any, map: MapShim) => void, thisArg?: any): void {
+    const { entries_ } = this;
+    forEach(entries_, (entry) => {
+      if (thisArg) {
+        callbackfn.call(thisArg, entry[1], entry[0], this);
+      }
+      else {
+        callbackfn(entry[1], entry[0], this);
+      }
+    });
+  }
+
+  get(key: any): any {
+    const { entries_ } = this;
+    const index = findIndex(entries_, key);
+    const entry = entries_[index];
+
+    return entry && entry[1];
+  }
+
+  has(key: any): boolean {
+    return !!~findIndex(this.entries_, key);
+  }
+
+  set(key: any, value: any): this {
+    const { entries_ } = this;
+    const index = findIndex(entries_, key);
+
+    if (~index) {
+      entries_[index][1] = value;
+    }
+    else {
+      entries_.push([key, value]);
+    }
+    return this;
+  }
+}
+
+export default !isUndefined(Map)
+  ? Map
+  : MapShim;
